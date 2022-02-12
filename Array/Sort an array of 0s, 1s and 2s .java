fe4248fe4dd31cb7a067473c37ba3e31b8af5317
BHARAT BHUSHAN ANAND <72190850+25Anand@users.noreply.github.com>
2022-02-13 00:02:10 +0530

    Problem _4

--- Array/Sort an array of 0s, 1s and 2s .java
+++ Array/Sort an array of 0s, 1s and 2s .java
@@ -1,8 +1,40 @@
+SOLUTION--1:
+
 class Solution
 {
     public static void sort012(int a[], int n)
     {
         // code here 
         Arrays.sort(a);
+    }
+}
+
+SOLUTION---2:
+    class Solution
+{
+    public static void sort012(int a[], int n)
+    {
+        // code here 
+        // Arrays.sort(a);
+        int low=0, mid=0, end=n-1;
+        while(mid<=end){
+            if (a[mid]==0){
+                int temp=a[low];
+                a[low]=a[mid];
+                a[mid]=temp;
+                    low++;
+                    mid++;
+                
+            }
+            else if(a[mid]==1){
+                    mid++;
+            }
+            else{
+                    int temp=a[end];
+                    a[end]=a[mid];
+                    a[mid]=temp;
+                    end--;
+            }
+        }
     }
 }
