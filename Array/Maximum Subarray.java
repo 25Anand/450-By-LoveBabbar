b405ab5dadb9527460bffc599402d90680d281a9
BHARAT BHUSHAN ANAND <72190850+25Anand@users.noreply.github.com>
2022-07-14 09:41:05 +0530

    A-3, M [Microsoft + Facebook Interview Qs]

---
+++ Array/Maximum Subarray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        
+        int cursum=0;
+        int maxsum=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            cursum=Math.max(cursum+nums[i],nums[i]);
+            maxsum=Math.max(maxsum,cursum);
+            
+        }
+        return maxsum;
+        
+    }
+}
